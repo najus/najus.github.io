2146edce96a9fe1eb5e26cb0ad25d48fb08c024a
Sujan <najussujan@gmail.com>
2015-12-26 17:39:20 -0500

    updated config file

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'a2615160_najus');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'a2615160_najus');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'najusdb123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'mysql5.000webhost.com');
